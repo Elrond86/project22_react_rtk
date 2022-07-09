af57e6fe8993e8bf9ef3adeca7b4339256b1e01a
Elrond <saCul@gmx.li>
2022-07-09 15:00:23 +0200

    added api to store

--- src/redux/store/Store.js
+++ src/redux/store/Store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import UIReducer from '../slices/ui/UISlice'
 import usersReducer from '../slices/users/UsersSlices'
+import api from '../utils/api'
 
 const store = configureStore({
 	reducer: {
 		UI: UIReducer,
 		users: usersReducer,
+		[api.reducerPath]: api.reducer
 	},
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api.middleware)
 })
 
 export default store
